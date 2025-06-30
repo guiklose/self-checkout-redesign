07dac55e278ff4ff939405cd35da24cec5a71c04
guiklose <guilhermeklosesantos@gmail.com>
2025-06-30 00:04:44 -0300

    Set price decimal cases to 2

--- src/app/components/Buy/CartSummary.tsx
+++ src/app/components/Buy/CartSummary.tsx
@@ -3,7 +3,7 @@
 export default function CartSummary({ total, count }: { total: number; count: number }) {
   return (
     <>
-      <p className="text-black">Total da Compra: R$ {total}</p>
+      <p className="text-black">Total da Compra: R$ {Number(total).toFixed(2)}</p>
       <p className="text-black">Quantidade de Itens Vendidos: {count}</p>
     </>
   );
