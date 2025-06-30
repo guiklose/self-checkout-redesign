96d3358aa8677f16f23ef5152f655fb40a743cb9
guiklose <guilhermeklosesantos@gmail.com>
2025-06-29 21:10:08 -0300

    Change text attribute to optional

--- src/app/components/ReturnButton.tsx
+++ src/app/components/ReturnButton.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-export default function ReturnButton({ route, text }: { route: string, text: string }) {
+export default function ReturnButton({ route, text }: { route: string, text?: string }) {
   const router = useRouter();
 
   return (
