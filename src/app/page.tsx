2dd024bed1390aee081ae65389ec6aecc5d84129
guiklose <guilhermeklosesantos@gmail.com>
2025-06-30 00:03:05 -0300

    Fix first page reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client";
+import { useEffect } from "react";
 
 import StartCheckout from "@/app/components/StartCheckout";
+import { useCart } from "@/app/context/CartContext";
 
 export default function Home() {
+  const { resetCart } = useCart();
+
+  useEffect(() => {
+    resetCart();
+  }, [resetCart]);
+
   return (
     <main>
       <StartCheckout />
